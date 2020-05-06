6897af3f520247c077183c231c2d500ee4841d63
Nathan Williams <npwilliams09@gmail.com>
2020-05-06 23:07:20 +1000

    added missing residue parser

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from parsers.textParser import textParser
-from parsers.targetParser import contactParser, distanceParser
+from parsers.targetParser import contactParser, distanceParser, parseStrippedRes
 
 def main():
-    threshold = 15
-    #contactParser("./PPI4DOCK/PPI4DOCK_docking_set/1a2y_AC/1a2y_AC_st.pdb",threshold)
-    distanceParser("./PPI4DOCK/PPI4DOCK_docking_set/1a2y_AC/1a2y_AC_st.pdb")
+    target = "1axi_BD"
+    prefix = "./PPI4DOCK/PPI4DOCK_docking_set/"
+    strippedRes = parseStrippedRes(prefix + f"{target}/stripped_res.txt")
+    distanceParser(prefix + f"{target}/{target}_st.pdb",strippedRes)
+
 
 
 if __name__ == '__main__':
--- parsers/targetParser.py
+++ parsers/targetParser.py
@@ -2,11 +2,12 @@ from Bio import PDB
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from math import pi
 
 '''
 Functions to parse single target complex into a contact or distance matrix
 '''
-''' REDUNDANT FUNCTION
+
 def contactParser(targetFile,threshold):
     parser = PDB.PDBParser()
     structure = parser.get_structure(targetFile[-14:-3],targetFile)
@@ -41,25 +42,28 @@ def contactParser(targetFile,threshold):
     plt.show()
 
     return mat
-'''
-def distanceParser(targetFile):
+
+def distanceParser(targetFile,strippedRes):
     parser = PDB.PDBParser()
     structure = parser.get_structure(targetFile[-14:-3], targetFile)
 
     model = structure[0]  # only one model
     chainIDs = [chain.id for chain in model]
 
-    dimensions = (len(model[chainIDs[0]]), len(model[chainIDs[1]]))
-    mat = np.zeros(shape=dimensions)
+    dimA = len(model[chainIDs[0]]) + strippedRes[0]
+    dimB = len(model[chainIDs[1]]) + strippedRes[1]
+    dimensions = (dimA,dimB)
+    # print(dimensions)
 
-    resA = model[chainIDs[0]][1]
-    resB = model[chainIDs[1]][2]
+    mat = np.zeros(shape=dimensions)
 
     for resA in model[chainIDs[0]]:
         for resB in model[chainIDs[1]]:
             mat[getSeqIndex(resA),getSeqIndex(resB)] = residueDistance(resA,resB)
     sns.heatmap(mat)
-    plt.show()
+
+
+    plt.savefig("distogram.png")
 
     return mat
 
@@ -69,9 +73,40 @@ def residueDistance(resA,resB):
 def centralCarbon(residue):
     if "CA" in residue:
         return residue["CA"]
-    else:
-        return residue["C"] #special case for glycine residues
+    else:#special case for glycine residues
+        #bipython code sample for virtual beta carbon
+        n = residue['N'].get_vector()
+        c = residue['C'].get_vector()
+        ca = residue['CA'].get_vector()
+
+        #center at origin
+        n = n-ca
+        c = c-ca
+        #find rotation matrix that rotates n
+        #-120 degrees along the ca-c vector
+        rot = PDB.rotaxis(-pi*120.0/180.0)
+        #apply rotation to ca-n vector
+        cb_at_origin = n.left_multiply(rot)
+        #put on top of ca atom
+        cb = cb_at_origin+ca
+        return cb
+
 
 def getSeqIndex(residue):
     ids = residue.get_id()
     return ids[1] - 1
+
+def parseStrippedRes(filePath):
+    with open(filePath) as file:
+        lineA = stripLine(file.readline())
+        lineB = stripLine(file.readline())
+        return (lineA,lineB)
+
+def stripLine(line):
+    res = []
+    for a in [x.strip() for x in line.split("\t")][1:]:
+        if (a != "None"):
+            ranges = [int(x) for x in a.split('-')]
+            res.extend(list(range(ranges[0], ranges[1] + 1)))
+    return len(res)
+

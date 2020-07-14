961ba48afc89c9e8a5ae06ad695a4f174990b4e7
Nathan Williams <npwilliams09@gmail.com>
2020-07-14 17:02:15 +1000

    Added Multithreading

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from utilities.dicNormaliser import dicNormaliser
 import os
 import numpy as np
 import gc
+from threading import Thread
 import pandas as pd
 
 def main():
+    Threads = 8
+
     makeFolder("./output")
     print("Parsing Text Files...")
     protInfo = textParser()
@@ -20,7 +23,7 @@ def main():
     train,test = trainTestSplit(protInfo)
 
     print("Load Train Set...")
-    trainDic = loadSet(train)
+    trainDic = threaded_process_range(train,Threads)
     loadTargets(train,trainDic)
 
     normCols = ["hsed","hseu","seqId","aligns",'res_depth','ca_depth']
@@ -33,7 +36,7 @@ def main():
     gc.collect()
 
     print("Load Test Set...")
-    testDic = loadSet(test)
+    testDic = threaded_process_range(test,Threads)
     loadTargets(train, trainDic)
 
     print("Normalise Test & Save...")
@@ -44,40 +47,49 @@ def main():
 
     print("Done, Output saved to folder :)")
 
-def loadSet(setList,breakPoint=100000):
+def loadProt(prot):
     master = {}
-    entries = min(len(setList),breakPoint)
-    makeFolder("./output/chains")
-    i = 0
-    surfaceDict = {}
+    makeFolder(f"./output/{prot}")
 
-    for prot in setList:
-        if prot[:-3] not in master:
-            master[prot[:-3]] = {}
-        chains = prot[-2:]
-        if chains[0] not in master[prot[:-3]]:
-            df = featureExtract(prot, chains[0])
-            df, ids = surfaceResidues(df)
-            adjMat = adjacencyMat(prot,chains[0],ids)
-            makeFolder(f"./output/chains/{prot[:-3]}_{chains[0]}")
-            with open(f"./output/chains/{prot[:-3]}_{chains[0]}/adjMat.npy", "wb") as f:
-                np.save(f, adjMat)
-            master[prot[:-3]][chains[0]] = df
-        if chains[1] not in master[prot[:-3]]:
-            df = featureExtract(prot, chains[1])
-            df, ids = surfaceResidues(df)
-            adjMat = adjacencyMat(prot, chains[1], ids)
-            makeFolder(f"./output/chains/{prot[:-3]}_{chains[1]}")
-            with open(f"./output/chains/{prot[:-3]}_{chains[1]}/adjMat.npy", "wb") as f:
-                np.save(f, adjMat)
-            master[prot[:-3]][chains[1]] = df
-        i+=1
-        print(f'\r>>Loading Set: {"{:.2f}".format(i / entries * 100)}%', end='')
-        if (i>=breakPoint):
-            print()
-            return master
-    print()
+    chains = prot[-2:]
+
+    master[chains[0]] = loadChain(prot,chains[0])
+    master[chains[1]] = loadChain(prot, chains[1])
+
     return master
+
+def loadChain(prot,chain):
+    df = featureExtract(prot, chain)
+    df, ids = surfaceResidues(df)
+    adjMat = adjacencyMat(prot, chain, ids)
+    with open(f"./output/{prot}/{chain}_adjMat.npy", "wb") as f:
+        np.save(f, adjMat)
+    return df
+
+def process_range(id_range, store=None):
+    """process a number of ids, storing the results in a dict"""
+    if store is None:
+        store = {}
+    for i, id in enumerate(id_range):
+        store[id] = loadProt(id)
+        print(f'\r>>Loading Set: {"{:.2f}".format(i+1 / len(id_range) * 100)}%', end='')
+    return store
+
+def threaded_process_range(id_range, nthreads=1):
+    """process the id range in a specified number of threads"""
+    store = {}
+    threads = []
+    # create the threads
+    for i in range(nthreads):
+        ids = id_range[i::nthreads]
+        t = Thread(target=process_range, args=(ids, store))
+        threads.append(t)
+
+    # start the threads
+    [t.start() for t in threads]
+    # wait for the threads to finish
+    [t.join() for t in threads]
+    return store
 
 def sizeFilter(protInfo,size=250000):
     newDic = {}
@@ -91,22 +103,19 @@ def loadTargets(complexes, protDic):
     end = len(complexes)
     i = 0
 
-    makeFolder("./output/distance")
-    makeFolder("./output/contact")
-
     for prot in complexes:
         filename = f"./PPI4DOCK/PPI4DOCK_docking_set/{prot}/{prot}_st.pdb"
         chains = prot[-2:]
         seqIDs = []
         for chain in chains:
-            df = protDic[prot[:-3]][chain]
+            df = protDic[prot][chain]
             seqIDs.append([int(x) for x in df["seqId"]])
 
         distance = distanceParser(filename,chains,seqIDs)
         contact = dis2contact(distance)
-        with open(f"./output/distance/{prot}_dist.npy","wb") as f:
+        with open(f"./output/{prot}/dist.npy","wb") as f:
             np.save(f,distance)
-        with open(f"./output/contact/{prot}_cont.npy","wb") as c:
+        with open(f"./output/{prot}/cont.npy","wb") as c:
             np.save(c,contact)
 
         i+=1
@@ -121,7 +130,7 @@ def saveSet(dic):
 
     for prot in dic:
         for chain in dic[prot]:
-            path = f"./output/chains/{prot}_{chain}/input.ft"
+            path = f"./output/{prot}/{chain}_input.ft"
             data = dic[prot][chain]
             data.to_feather(path)
         i += 1
